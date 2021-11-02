ca8d98b6f3a63a48520ceb81158356ed8b2b4a02
Ankur Saxena <ankursaxenaankur0@gmail.com>
2021-11-02 16:11:04 +0530

    Added to new project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react"
 // import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/js/bootstrap.min.js'
@@ -28,3 +29,30 @@ function App(){
 }
 
 export default App;
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+>>>>>>> ed9a0db (Initialize project using Create React App)
