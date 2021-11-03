1627a6e2a08edc05de2ba086139181a9c800dc0c
Ankur Saxena <ankursaxenaankur0@gmail.com>
2021-11-03 15:46:55 +0530

    signup and login page added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.min.js'
+import Header1 from "./components/header 1";
+import Header2 from "./components/header 2";
+import Home from "./components/home.js";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom"
+import Login from "./components/login";
+import SignUp from "./components/signup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    console.log(window.innerHeight);
+    return(
+        <div>
+            
+                    <Route exact path="/">
+                        <Header1 />
+                        <Home/>
+                    </Route>
+                    <Route exact path="/login">
+                        <Login/>
+                    </Route>
+                    <Route exact path="/signup">
+                        <SignUp/>
+                    </Route>
+        </div>
+    )
 }
 
 export default App;
