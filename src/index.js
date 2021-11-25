6b2ac4c7e2b6d5c7a2035d7ea1634f3aefb90b7f
ankur6266 <ankur@LAPTOP-B9VT9UQ2.localdomain>
2021-11-25 19:09:44 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import reactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app";
+import App from "./App";
 
 reactDom.render(
     <BrowserRouter>
